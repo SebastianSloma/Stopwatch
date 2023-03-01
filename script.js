238ec562ec09d6a681d94a4c3778eb833e60d491
Sebastian <sebastiansloma@gmail.com>
2023-03-01 17:44:43 +0100

    added functional start button

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ const timeList = document.querySelector('.time-list')
 const infoBtn = document.querySelector('.info')
 const modalShadow = document.querySelector('.modal-shadow')
 const closeModalBtn = document.querySelector('.close')
+
+let countTime
+let minutes = 0 
+let seconds = 0
+
+const handleStart = () => {
+
+    countTime = setInterval(()=>{
+
+        if ( seconds < 9){
+            seconds++
+            console.log(seconds);
+            stopwatch.textContent = `${minutes}:0${seconds}`
+
+    }else if (seconds >= 9 && seconds <59){
+        seconds++
+        console.log(`SECOND IF: ${seconds}`);
+        stopwatch.textContent = `${minutes}:${minutes}`
+
+    }else {
+        minutes++
+        seconds=0
+        stopwatch.textContent = `${minutes}:00`
+    }
+
+    
+
+    },500)
+}
+
+startBtn.addEventListener('click', handleStart)
