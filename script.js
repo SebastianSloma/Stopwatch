e6fb0a6a6bf0544835fe711aea1e81e9a58f3d97
Sebastian <sebastiansloma@gmail.com>
2023-03-02 18:41:00 +0100

    added change colors

--- script.js
+++ script.js
@@ -7,9 +7,18 @@ const stopwatch = document.querySelector('.stopwatch');
 const time = document.querySelector('.time');
 const timeList = document.querySelector('.time-list');
 
-const infoBtn = document.querySelector('.info');
+const infoBtn = document.querySelector('.fa-question');
 const modalShadow = document.querySelector('.modal-shadow');
 const closeModalBtn = document.querySelector('.close');
+
+const colorBtn = document.querySelector('.fa-paintbrush');
+const colorPanel = document.querySelector('colors')
+
+const colorOne = document.querySelector('one');
+const colorTwo = document.querySelector('two');
+const colorThree = document.querySelector('three');
+const colorFour = document.querySelector('four');
+let root = document.documentElement;
 
 let countTime;
 let minutes = 0;
@@ -66,29 +75,27 @@ const clearStuff = () => {
 };
 
 const showHistory = () => {
-    timeList.textContent = '';
-    let num =1
+	timeList.textContent = '';
+	let num = 1;
 
-	timesArr.forEach(time => {
+	timesArr.forEach((time) => {
 		const newTime = document.createElement('li');
 		newTime.innerHTML = `Measurement no. ${num}: <span>${time}</span>`;
 
 		timeList.appendChild(newTime);
-        num++
+		num++;
 	});
 };
 
+const showModal = () => {
+	if (!(modalShadow.style.display === 'block')) {
+		modalShadow.style.display = 'block';
+	} else {
+		modalShadow.style.display = 'none';
+	}
 
-const showModal =() =>{
-    if(!(modalShadow.style.display === 'block')){
-        modalShadow.style.display ='block'
-    }else{
-        modalShadow.style.display ='none'
-    }
-
-    modalShadow.classList.toggle('modal-animation')
-
-}
+	modalShadow.classList.toggle('modal-animation');
+};
 
 startBtn.addEventListener('click', handleStart);
 pauseBtn.addEventListener('click', handlePause);
@@ -96,5 +103,25 @@ stopBtn.addEventListener('click', handleStop);
 resetBtn.addEventListener('click', handleReset);
 historyBtn.addEventListener('click', showHistory);
 infoBtn.addEventListener('click', showModal);
-closeModalBtn.addEventListener('click', showModal)
-window.addEventListener('click', e=>e.target ===modalShadow ? showModal() : false)
+closeModalBtn.addEventListener('click', showModal);
+window.addEventListener('click', (e) =>
+	e.target === modalShadow ? showModal() : false
+);
+
+
+colorBtn.addEventListener('click', () =>{
+	colorPanel.classList.toggle('show-colors')
+})
+
+colorOne.addEventListener('click', () => {
+	root.style.setProperty('--first-color', 'red');
+});
+colorTwo.addEventListener('click', () => {
+	root.style.setProperty('--first-color', 'royalblue');
+});
+colorThree.addEventListener('click', () => {
+	root.style.setProperty('--first-color', 'greenyellow');
+});
+colorFour.addEventListener('click', () => {
+	root.style.setProperty('--first-color', 'gold');
+});
