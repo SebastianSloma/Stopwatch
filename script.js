a79a326e970a3846f225593c9f3d97162c746484
Sebastian <sebastiansloma@gmail.com>
2023-03-02 15:58:01 +0100

    added pause button func

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-const startBtn = document.querySelector('.start')
-const pauseBtn = document.querySelector('.pause')
-const stopBtn = document.querySelector('.stop')
-const resetBtn = document.querySelector('.reset')
-const historyBtn = document.querySelector('.history')
-const stopwatch = document.querySelector('.stopwatch')
-const time = document.querySelector('.time')
-const timeList = document.querySelector('.time-list')
+const startBtn = document.querySelector('.start');
+const pauseBtn = document.querySelector('.pause');
+const stopBtn = document.querySelector('.stop');
+const resetBtn = document.querySelector('.reset');
+const historyBtn = document.querySelector('.history');
+const stopwatch = document.querySelector('.stopwatch');
+const time = document.querySelector('.time');
+const timeList = document.querySelector('.time-list');
 
-const infoBtn = document.querySelector('.info')
-const modalShadow = document.querySelector('.modal-shadow')
-const closeModalBtn = document.querySelector('.close')
+const infoBtn = document.querySelector('.info');
+const modalShadow = document.querySelector('.modal-shadow');
+const closeModalBtn = document.querySelector('.close');
 
-let countTime
-let minutes = 0 
-let seconds = 0
+let countTime;
+let minutes = 0;
+let seconds = 0;
 
 const handleStart = () => {
+	clearInterval(countTime);
 
-    countTime = setInterval(()=>{
+	countTime = setInterval(() => {
+		if (seconds < 9) {
+			seconds++;
+			stopwatch.textContent = `${minutes}:0${seconds}`;
+		} else if (seconds >= 9 && seconds < 59) {
+			seconds++;
+			stopwatch.textContent = `${minutes}:${seconds}`;
+		} else {
+			minutes++;
+			seconds = 0;
+			stopwatch.textContent = `${minutes}:00`;
+		}
+	}, 1000);
+};
 
-        if ( seconds < 9){
-            seconds++
-            console.log(seconds);
-            stopwatch.textContent = `${minutes}:0${seconds}`
+const handlePause = () => {
+	clearInterval(countTime);
+};
 
-    }else if (seconds >= 9 && seconds <59){
-        seconds++
-        console.log(`SECOND IF: ${seconds}`);
-        stopwatch.textContent = `${minutes}:${minutes}`
-
-    }else {
-        minutes++
-        seconds=0
-        stopwatch.textContent = `${minutes}:00`
-    }
-
-    
-
-    },500)
-}
-
-startBtn.addEventListener('click', handleStart)
+startBtn.addEventListener('click', handleStart);
+pauseBtn.addEventListener('click', handlePause);
+pauseBtn.addEventListener('click', handlePause);
